926769555fbd469b51653e82b6bc0cdd550d2d6e
jersonval96 <jersonDevelopment@gmail.com>
2021-09-24 13:08:36 -0500

    created datasource and models

---
+++ src/models/factura.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Factura extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  consecutivo: number;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fecha: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  hora: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  id_cliente: number;
+
+
+  constructor(data?: Partial<Factura>) {
+    super(data);
+  }
+}
+
+export interface FacturaRelations {
+  // describe navigational properties here
+}
+
+export type FacturaWithRelations = Factura & FacturaRelations;
---
+++ src/models/producto-factura.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class ProductoFactura extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  precio: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  id_producto: number;
+
+
+  constructor(data?: Partial<ProductoFactura>) {
+    super(data);
+  }
+}
+
+export interface ProductoFacturaRelations {
+  // describe navigational properties here
+}
+
+export type ProductoFacturaWithRelations = ProductoFactura & ProductoFacturaRelations;
